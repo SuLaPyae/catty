17a339dce3691ffff62de3f877ad8868172e60de
sulapyae2017@gmail.com <sulapyae@Sus-MacBook-Pro.local>
2020-12-14 21:09:59 +0630

    catttt

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,40 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-      
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        tooltipArrows: theme => ({
+            'danger-arrow': {
+                borderColor: theme('colors.red.400'),
+                borderWidth: 1,
+                backgroundColor: theme('colors.red.200'),
+                size: 10,
+                offset: 10
+            },
+        }),
+        extend: {
+
+        },
+        zIndex: {
+            auto: 'auto',
+            'negative': '-1',
+            0: '0',
+            10: '10',
+            20: '20',
+            30: '30',
+            40: '40',
+            50: '50',
+        },
+
     },
-  },
-  variants: {
-    extend: {
-      maxHeight: ['focus'],    },
-  },
-  plugins: [],
+    variants: {
+        extend: {
+            maxHeight: ['focus'],
+        },
+    },
+    plugins: [
+        require('tailwindcss-elevation')(['responsive']),
+
+        require('tailwindcss-tooltip-arrow-after')(),
+
+    ],
 }
